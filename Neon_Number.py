4d8b49a995c89dd92c55d0bfd969dac1cc32d4ca
gayatritalluri <119866807+gayatritalluri@users.noreply.github.com>
2023-06-02 18:33:36 +0530

    n=int(input()) s=n*n p=0 for i in str(s):     p=p+int(i) if(p==n):     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,11 +1,9 @@
-num = int(input())
-sqr = num*num 
-sumOfDigit = 0
-while sqr>0:
-    sumOfDigit =sumOfDigit + sqr%10
-    sqr = sqr//10
-
-if (num == sumOfDigit):
+n=int(input())
+s=n*n
+p=0
+for i in str(s):
+    p=p+int(i)
+if(p==n):
     print("Neon Number")
 else:
     print("Not Neon Number")
